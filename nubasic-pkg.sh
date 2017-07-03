b16c51397a50547cb56c66072105f2c581e3913b
Antonino Calderone <antonino.calderone@gmail.com>
2017-07-03 18:50:47 +0100

    Update dist content

--- nubasic-pkg.sh
+++ nubasic-pkg.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo checkinstall --pkgrelease=1.48 --pkgversion=1 --pkglicense="MIT" -y --maintainer="Antonino Calderone antonino.calderone@gmail.com" make install
+./configure --enable-gtkide
+sudo checkinstall --pkgrelease=1.48 --pkgversion=1 --strip=yes --stripso=yes --addso=yes --pkglicense="MIT" -y --maintainer="Antonino Calderone antonino.calderone@gmail.com" make install
