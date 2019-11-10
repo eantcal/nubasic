6d50dd220174dc50247c5ab1f4469bf6b0496828
Antonino <antonino.calderone@gmail.com>
2019-11-10 16:08:16 +0000

    Remove autotools files

--- nubasic-pkg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./configure --enable-gtkide
-sudo checkinstall --pkgrelease=1.48 --pkgversion=1 --strip=yes --stripso=yes --addso=yes --pkglicense="MIT" -y --maintainer="Antonino Calderone antonino.calderone@gmail.com" make install
